db022a224c2416315bcd0c18bb1f108bba29759c
Khoa Nguyen <nguyenthoaidangkhoa@gmail.com>
2024-03-06 15:25:46 +0700

    [FEAT]: HomeScreen draft

--- lib/ui/homepage/home_screen.dart
+++ lib/ui/homepage/home_screen.dart
@@ -1,9 +1,15 @@
+import 'dart:ui';
+
 import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:studenthub/blocs/theme_bloc/theme_bloc.dart';
 import 'package:studenthub/blocs/theme_bloc/theme_event.dart';
 import 'package:studenthub/constants/key_translator.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:studenthub/ui/homepage/widgets/my_elavated_button.dart';
 
 class HomePageScreen extends StatelessWidget {
   const HomePageScreen({super.key});
@@ -11,44 +17,127 @@ class HomePageScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "Trang chủ",
-        ),
-      ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          ElevatedButton(
-            onPressed: () {
-              context.setLocale(const Locale('en'));
-              context.read<ThemesBloc>().add(ToggleLanguageEvent(context));
-            },
+        children: [
+          Container(
+            margin: const EdgeInsets.all(24),
+            child: AppBar(
+              title: Text('StudentHub',
+                  style: GoogleFonts.martelSans(
+                      fontSize: 36,
+                      color: const Color(0xFF6188FF),
+                      fontWeight: FontWeight.bold)),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 36),
+            // decoration: const BoxDecoration(color: Colors.black),
+            child: Transform.scale(
+              scale: 1.4,
+              child: Image.asset(
+                'lib/assets/images/landing.png',
+              ),
+            ),
+          ),
+          const SizedBox(height: 48),
+          SizedBox(
+            width: 375,
             child: Text(
-              englishKey.tr().toUpperCase(),
+              'Build your product with high-skilled student',
+              style: GoogleFonts.inter(
+                fontSize: 16,
+                color: const Color(0xFF848484),
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
             ),
           ),
-          ElevatedButton(
-            onPressed: () {
-              context.setLocale(const Locale('vi'));
-              context.read<ThemesBloc>().add(ToggleLanguageEvent(context));
-            },
+          const SizedBox(height: 8),
+          SizedBox(
+            width: 375,
             child: Text(
-              vietnamesKey.tr().toUpperCase(),
+              'Find and onboard top-tier students for your projects, enabling them to gain real-world experience and skills',
+              style: GoogleFonts.inter(
+                fontSize: 14,
+                color: const Color(0xFF848484),
+                fontWeight: FontWeight.w400,
+                height: 1.3, // Adjust the line height here
+              ),
+              textAlign: TextAlign.center,
             ),
           ),
-          ElevatedButton(
-            onPressed: () {
-              context.read<ThemesBloc>().add(ToggleThemeEvent());
-            },
-            child: const Text(
-              "Chuyển đổi theme",
-            ),
-          ),
+          const SizedBox(height: 40),
           Center(
-            child: Text(
-              hiKey.tr(),
-              style: const TextStyle(fontSize: 20),
+            child: MyElevatedButton(
+              height: 45,
+              width: 350,
+              gradient: const LinearGradient(
+                colors: [Color(0xFFACC0FF), Color(0xFF6188FF)],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+              onPressed: () {},
+              borderRadius: BorderRadius.circular(20),
+              child: Text(
+                'company'.toUpperCase(),
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 12),
+          Center(
+            child: MyElevatedButton(
+              height: 45,
+              width: 350,
+              backgroundColor: Colors.transparent,
+              borderRadius: BorderRadius.circular(20),
+              buttonBorder: Border.all(
+                color: const Color(0xFF6188FF),
+                width: 2,
+              ),
+              onPressed: () {},
+              child: Text(
+                'student'.toUpperCase(),
+                style: const TextStyle(
+                  color: Color(0xFF6188FF),
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+          const Spacer(),
+          Container(
+            margin: const EdgeInsets.all(32),
+            child: SizedBox(
+              width: 350,
+              child: Text.rich(
+                TextSpan(
+                  children: [
+                    TextSpan(
+                      text:
+                          'When proceeding to the next step, I agreed to the ',
+                      style: GoogleFonts.inter(
+                        fontSize: 14,
+                        color: const Color(0xFF848484),
+                      ),
+                    ),
+                    TextSpan(
+                      text: 'Terms of Service and Usage Policy.',
+                      style: GoogleFonts.inter(
+                        fontSize: 14,
+                        color: const Color(0xFF848484),
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+                textAlign: TextAlign.center,
+              ),
             ),
           ),
         ],
