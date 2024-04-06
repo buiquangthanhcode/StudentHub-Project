94782684115b0984d0e3bd44d3cc2c52b4e9ee88
khangduy017 <khangduy017@gmail.com>
2024-04-06 16:37:28 +0700

    [Feat] update company create profile bloc

--- lib/models/company/company_model.dart
+++ lib/models/company/company_model.dart
@@ -1,49 +1,9 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:convert';
 
 class Company {
+  String? employeeQuantity;
   String? name;
-  Company({
-    this.name,
-  });
-
-  Company copyWith({
-    String? name,
-  }) {
-    return Company(
-      name: name ?? this.name,
-    );
-  }
-
-  Map<String, dynamic> toMap() {
-    final result = <String, dynamic>{};
-
-    if (name != null) {
-      result.addAll({'name': name});
-    }
-
-    return result;
-  }
-
-  factory Company.fromMap(Map<String, dynamic> map) {
-    return Company(
-      name: map['name'],
-    );
-  }
-
-  String toJson() => json.encode(toMap());
-
-  factory Company.fromJson(String source) => Company.fromMap(json.decode(source));
-
-  @override
-  String toString() => 'Company(name: $name)';
-
-  @override
-  bool operator ==(Object other) {
-    if (identical(this, other)) return true;
-
-    return other is Company && other.name == name;
-  }
-
-  @override
-  int get hashCode => name.hashCode;
+  String? website;
+  String? description;
 }
--- lib/services/endpoint.dart
+++ lib/services/endpoint.dart
@@ -1,1 +1,1 @@
-const String baseURL = "http://192.168.1.155:4400/";
+const String baseURL = "http://34.16.137.128:4400/";
