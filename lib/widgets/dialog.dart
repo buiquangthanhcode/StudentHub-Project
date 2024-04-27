c3d0ab53f998cbca3e630575225ea40196663967
khangduy017 <khangduy017@gmail.com>
2024-04-27 17:28:51 +0700

    [Refactor] Refactor button "Change your account"

--- lib/widgets/dialog.dart
+++ lib/widgets/dialog.dart
@@ -3,7 +3,12 @@ import 'package:studenthub/constants/app_theme.dart';
 import 'package:studenthub/constants/colors.dart';
 
 Future<void> showDialogCustom(BuildContext context,
-    {String? image, String? title, String? subtitle, Function? onSave, String? textButtom, double? sizeImage}) async {
+    {String? image,
+    String? title,
+    String? subtitle,
+    Function? onSave,
+    String? textButtom,
+    double? sizeImage}) async {
   TextTheme textTheme = Theme.of(context).textTheme;
   var colorTheme = Theme.of(context).colorScheme;
   showDialog(
@@ -17,7 +22,9 @@ Future<void> showDialogCustom(BuildContext context,
                 horizontal: 20,
                 vertical: 20,
               ),
-              decoration: const BoxDecoration(color: Colors.white, borderRadius: BorderRadius.all(Radius.circular(15))),
+              decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(15))),
               child: Stack(
                 clipBehavior: Clip.antiAlias,
                 children: [
@@ -43,15 +50,20 @@ Future<void> showDialogCustom(BuildContext context,
                         height: 10,
                       ),
                       Text(
-                        subtitle ?? 'Start searching and implementing real-world projects right now!',
+                        subtitle ??
+                            'Start searching and implementing real-world projects right now!',
                         textAlign: TextAlign.center,
-                        style: textTheme.bodySmall!.copyWith(color: colorTheme.grey),
+                        style: textTheme.bodySmall!
+                            .copyWith(color: colorTheme.grey),
                       ),
                       const SizedBox(
                         height: 10,
                       ),
                       ElevatedButton(
                         style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
                           minimumSize: const Size(double.infinity, 56),
                           backgroundColor: primaryColor,
                         ),
@@ -60,7 +72,8 @@ Future<void> showDialogCustom(BuildContext context,
                         },
                         child: Text(
                           textButtom ?? 'Get Started!',
-                          style: textTheme.bodyMedium!.copyWith(color: Colors.white, fontWeight: FontWeight.w600),
+                          style: textTheme.bodyMedium!.copyWith(
+                              color: Colors.white, fontWeight: FontWeight.w600),
                         ),
                       ),
                     ],
@@ -75,8 +88,11 @@ Future<void> showDialogCustom(BuildContext context,
                       child: Container(
                         margin: const EdgeInsets.all(5),
                         decoration: BoxDecoration(
-                          color: Theme.of(context).colorScheme.grey?.withOpacity(0.5),
-                          borderRadius: BorderRadius.circular(50),
+                          color: Theme.of(context)
+                              .colorScheme
+                              .grey
+                              ?.withOpacity(0.5),
+                          borderRadius: BorderRadius.circular(10),
                         ),
                         child: InkWell(
                           onTap: () {
