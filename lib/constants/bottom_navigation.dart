e10091fec069f91b469cd4338675f16780f0c734
Khoa Nguyen <nguyenthoaidangkhoa@gmail.com>
2024-04-28 16:28:01 +0700

    [Feat] Translate for navigation bar

--- lib/constants/bottom_navigation.dart
+++ lib/constants/bottom_navigation.dart
@@ -1,27 +1,30 @@
-const bottomNavs = [
+import 'package:easy_localization/easy_localization.dart';
+import 'package:studenthub/constants/key_translator.dart';
+
+final bottomNavs = [
   {
     'solid-icon': 'lib/assets/nav_icons/solid/rectangles-mixed.svg',
     'regular-icon': 'lib/assets/nav_icons/regular/rectangles-mixed.svg',
-    'title': 'Dashboard',
+    'title': dashboardNavKey.tr(),
   },
   {
     'solid-icon': 'lib/assets/nav_icons/solid/ballot-check.svg',
     'regular-icon': 'lib/assets/nav_icons/regular/ballot-check.svg',
-    'title': 'Projects',
+    'title': projectsNavKey.tr(),
   },
   {
     'solid-icon': 'lib/assets/nav_icons/solid/messages.svg',
     'regular-icon': 'lib/assets/nav_icons/regular/messages.svg',
-    'title': 'Messages',
+    'title': messagesNavKey.tr(),
   },
   {
     'solid-icon': 'lib/assets/nav_icons/solid/bell.svg',
     'regular-icon': 'lib/assets/nav_icons/regular/bell.svg',
-    'title': 'Alerts',
+    'title': alertsNavKey.tr(),
   },
   {
     'solid-icon': 'lib/assets/nav_icons/solid/circle-user.svg',
     'regular-icon': 'lib/assets/nav_icons/regular/circle-user.svg',
-    'title': 'Account',
+    'title': accountNavKey.tr(),
   },
 ];
